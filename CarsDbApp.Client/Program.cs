444232eea32346f452a3a41648b4791333871272
Bedő Sebestyén <bedosebestyen@gmail.com>
2022-10-12 18:32:41 +0200

    clienten kívül fullos

--- CarsDbApp.Client/Program.cs
+++ CarsDbApp.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarsDbApp.Logic;
+using CarsDbApp.Repository;
+using System;
 
 namespace CarsDbApp.Client
 {
@@ -7,6 +9,18 @@ namespace CarsDbApp.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var ctx = new CarsDbContext();
+
+            var carRepo = new CarsRepository(ctx);
+            var carShopRepo = new CarShopRepository(ctx);
+            var customerRepo = new CustomerRepository(ctx);
+
+            var carLogic = new CarsLogic(carRepo);
+            var carShopLogic = new CarShopLogic(carShopRepo);
+            var customerLogic = new CustomerLogic(customerRepo);
+
+            var items = carLogic.ReadAll();
+            ;
         }
     }
 }
